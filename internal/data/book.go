978d43e000bca18c0c0c38f46b0c4aa0d01a5073
slbmax <slobodzian.maxim@gmail.com>
2022-11-07 17:49:40 +0200

    updated book resources

--- internal/data/book.go
+++ internal/data/book.go
@@ -16,19 +16,21 @@ type BookQ interface {
 }
 
 type Book struct {
-	ID              int64     `db:"id" structs:"-"`
-	Title           string    `db:"title" structs:"title"`
-	Description     string    `db:"description" structs:"description"`
-	CreatedAt       time.Time `db:"created_at" structs:"created_at"`
-	Price           string    `db:"price" structs:"price"`
-	ContractAddress string    `db:"contract_address" structs:"contract_address"`
-	ContractName    string    `db:"contract_name" structs:"contract_name"`
-	ContractSymbol  string    `db:"contract_symbol" structs:"contract_symbol"`
-	ContractVersion string    `db:"contract_version" structs:"contract_version"`
-	Banner          string    `db:"banner" structs:"banner"`
-	File            string    `db:"file" structs:"file"`
-	Deleted         bool      `db:"deleted" structs:"-"`
-	LastBlock       uint64    `db:"last_block" structs:"last_block"`
+	ID              int64                       `db:"id" structs:"-"`
+	Title           string                      `db:"title" structs:"title"`
+	Description     string                      `db:"description" structs:"description"`
+	CreatedAt       time.Time                   `db:"created_at" structs:"created_at"`
+	Price           string                      `db:"price" structs:"price"`
+	ContractAddress string                      `db:"contract_address" structs:"contract_address"`
+	ContractName    string                      `db:"contract_name" structs:"contract_name"`
+	ContractSymbol  string                      `db:"contract_symbol" structs:"contract_symbol"`
+	ContractVersion string                      `db:"contract_version" structs:"contract_version"`
+	Banner          string                      `db:"banner" structs:"banner"`
+	File            string                      `db:"file" structs:"file"`
+	Deleted         bool                        `db:"deleted" structs:"-"`
+	TokenId         int64                       `db:"token_id" structs:"token_id"`
+	DeployStatus    book_resources.DeployStatus `db:"deploy_status" structs:"deploy_status"`
+	LastBlock       uint64                      `db:"last_block" structs:"last_block"`
 }
 
 func (b *Book) Resource() (*book_resources.Book, error) {
@@ -53,6 +55,8 @@ func (b *Book) Resource() (*book_resources.Book, error) {
 			ContractVersion: b.ContractVersion,
 			File:            media[1],
 			Banner:          media[0],
+			TokenId:         int32(b.TokenId),
+			DeployStatus:    b.DeployStatus,
 		},
 	}
 
