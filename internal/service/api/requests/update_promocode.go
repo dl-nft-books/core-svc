ea8539ed2fee68b02b61d0f4bfba5a52d0b36b53
anastasiia.lishchenko <anastasiia.lishchenko@gmail.com>
2022-12-22 16:30:40 +0200

    Fix update project request

--- internal/service/api/requests/update_promocode.go
+++ internal/service/api/requests/update_promocode.go
@@ -33,17 +33,14 @@ func (r UpdatePromocodeRequest) validate() error {
 	return validation.Errors{
 		"data/attributes/discount": validation.Validate(
 			r.Data.Attributes.Discount,
-			validation.Required,
 			validation.Min(0.0),
 			validation.Max(1.0),
 		),
 		"data/attributes/initial_usages": validation.Validate(
 			r.Data.Attributes.InitialUsages,
-			validation.Required,
 		),
 		"data/attributes/expiration_date": validation.Validate(
 			r.Data.Attributes.ExpirationDate,
-			validation.Required,
 		),
 	}.Filter()
 }
